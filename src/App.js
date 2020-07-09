7afb3b0108f0a0b49427fe0f1b47b12ab77cf516
Julian Rudolph <julianrudolph@Julians-MacBook-Air.local>
2020-07-09 11:04:55 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import HavenCalc from './HavenCalc';
+import './normalize.css';
+import './skeleton.css';
 import './App.css';
 
 function App() {
--- src/HavenCalc.js
+++ src/HavenCalc.js
@@ -36,20 +36,41 @@ class HavenCalc extends Component{
     let remainingOffall = (mathOffall - lostFromCut).toFixed(3);
 
     return(
-      <div>
-        <form>
-          <label>Input Part Length: </label>
-          <input onChange={this.handleChange} type="number" name="pLength" value={partLength} />
-          <label>Input Longbar Length: </label>
-          <input onChange={this.handleChange} type="number" name="lbLength" value={lbLength} />
+      <div class="container">
+        <form class="six columns">
+              <label>Input Part Length: </label>
+              <input onChange={this.handleChange} type="number" name="pLength" value={partLength} />
+              <label>Input Longbar Length: </label>
+              <input onChange={this.handleChange} type="number" name="lbLength" value={lbLength} />
         </form>
-
-        <h3>Longbar Length: {lbLength}</h3>
-        <h3>Part Length: {partLength}</h3>
-        <p><strong>Whole Parts: </strong>{wholeParts}</p>
-        <p><strong>Math Offall: </strong>{mathOffall}</p>
-        <p><strong>Lost From Cut: </strong>{lostFromCut}</p>
-        {remainingOffall < 2 || remainingOffall >= 4 ? <p class="red">{remainingOffall} Offall too large or too small.</p> : <p class="green">{remainingOffall} offall fits params!</p>}
+        <h2 class="six columns">Haven Calc 3k</h2>
+        <ul>
+          <li>.125 (1/8th inch) is lost per cut from verticle blade.</li>
+          <li>Aim for 2-3 inch left over. Less may destroy last piece.</li>
+          <li>Arrow UP or DOWN and watch magic happen.</li>
+        </ul>
+        <table class="u-full-width">
+          <thead>
+            <tr>
+              <th>Part Length</th>
+              <th>Longbar Length</th>
+              <th>Whole Parts</th>
+              <th>Math Offall</th>
+              <th>Lost from Cut</th>
+              <th>Remaining Offall</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{partLength}</td>
+              <td>{lbLength}</td>
+              <td>{wholeParts}</td>
+              <td>{mathOffall}</td>
+              <td>{lostFromCut}</td>
+              {remainingOffall < 2 || remainingOffall > 4 ? <td class='red'>{remainingOffall}</td> : <td class='green'>{remainingOffall}</td>}
+            </tr>
+          </tbody>
+        </table>
       </div>
 
 
